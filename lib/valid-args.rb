77f439542d8b083d50581205253b98b504bc5402
Riki Ueno <31506@toyota.kosen-ac.jp>
2019-01-07 09:19:51 +0900

    coudn't change module but, can change 'class << self'

--- lib/valid-args.rb
+++ lib/valid-args.rb
@@ -2,31 +2,33 @@ require_relative '../models/mode'
 require_relative '../models/user'
 
 class ValidArgs < ActiveRecord::Base
-  def self.is_args_larger_than_number?(args, number)
-    return true if args.length > number
-    return false
-  end
-
-  def self.is_args_smaller_than_number?(args, number)
-    return true if args.length < number
-    return false
-  end
-
-  def self.is_args_equal_number?(args, number)
-    return true if args.length == number
-    return false
-  end
-
-  def self.is_exist_mode?(mode)
-    return true if Mode.find_by(play_mode: mode)
-    return false
-  end
-
-  def self.is_exist_player?(players_name)
-    players_name.each do |player_name|
-      return false if User.find_by(player_name: player_name).nil?
+  class << self
+    def is_args_larger_than_number?(args, number)
+      return true if args.length > number
+      return false
     end
-    return true
+
+    def is_args_smaller_than_number?(args, number)
+      return true if args.length < number
+      return false
+    end
+
+    def is_args_equal_number?(args, number)
+      return true if args.length == number
+      return false
+    end
+
+    def is_exist_mode?(mode)
+      return true if Mode.find_by(play_mode: mode)
+      return false
+    end
+
+    def is_exist_player?(players_name)
+      players_name.each do |player_name|
+        return false if User.find_by(player_name: player_name).nil?
+      end
+      return true
+    end
   end
 end
 
