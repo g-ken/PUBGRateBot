9b0f7b15ee7d1bd7a9df45b8ce2d75f78fa6ceba
Riki Ueno <31506@toyota.kosen-ac.jp>
2019-01-07 09:39:02 +0900

    create-table refactaringed 'class << self'

--- lib/create-table.rb
+++ lib/create-table.rb
@@ -1,27 +1,29 @@
 require_relative "../models/mode"
 class CreateTable < ActiveRecord::Base
   #ＤＢがなければ作成
-  def self.create(database_name)
-    unless File.exist?(database_name)
-      ActiveRecord::Migration.create_table :rates do |t|
-        t.integer :player_rate
-        t.integer :user_id
-        t.integer :mode_id
-        t.timestamps
-      end
+  class << self
+    def create(database_name)
+      unless File.exist?(database_name)
+        ActiveRecord::Migration.create_table :rates do |t|
+          t.integer :player_rate
+          t.integer :user_id
+          t.integer :mode_id
+          t.timestamps
+        end
 
-      ActiveRecord::Migration.create_table :users do |t|
-        t.string :player_name
-        t.string :player_id
-        t.timestamps
-      end
+        ActiveRecord::Migration.create_table :users do |t|
+          t.string :player_name
+          t.string :player_id
+          t.timestamps
+        end
 
-      ActiveRecord::Migration.create_table :modes do |t|
-        t.string :play_mode
-      end
+        ActiveRecord::Migration.create_table :modes do |t|
+          t.string :play_mode
+        end
 
-      ["solo", "solo-fpp", "duo", "duo-fpp", "squad", "squad-fpp"].each do | mode |
-        Mode.new(play_mode: mode).save
+        ["solo", "solo-fpp", "duo", "duo-fpp", "squad", "squad-fpp"].each do | mode |
+          Mode.new(play_mode: mode).save
+        end
       end
     end
   end
