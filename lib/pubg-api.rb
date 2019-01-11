c83d8842f7ef270ea5f9388b7acc2e3c36227d1f
Riki Ueno <31506@toyota.kosen-ac.jp>
2019-01-11 09:28:18 +0900

    refactaring PUBGApi

--- lib/pubg-api.rb
+++ lib/pubg-api.rb
@@ -1,40 +1,36 @@
 module PUBGApi
   #リクエストＵＲＬの先頭につける
   BASE_URL = "https://api.pubg.com/shards/steam/"
+  class << self
+    #名前からＩＤを調べたりするのに使う
+    def get_player_stats(api_key, player_name)
+      url = BASE_URL + "players?filter[playerNames]=" + player_name
+      return request_data(api_key, url)
+    end
 
-  #名前からＩＤを調べたりするのに使う
-  def get_player_stats(api_key, player_name)
-    url = BASE_URL + "players?filter[playerNames]=" + player_name
-    return request_data(api_key, url)
-  end
-  module_function :get_player_stats
+    #シーズンデータを取得する
+    def get_seasons(api_key)
+      url = BASE_URL + "seasons"
+      return request_data(api_key, url)
+    end
 
-  #シーズンデータを取得する
-  def get_seasons(api_key)
-    url = BASE_URL + "seasons"
-    return request_data(api_key, url)
-  end
-  module_function :get_seasons
+    #プレイヤーのシーズンでのデータを取得する
+    def get_player_season_stats(api_key, player_id, season_id)
+      url = BASE_URL + "players/" + player_id + "/seasons/" + season_id.to_s
+      return request_data(api_key, url)
+    end
 
-  #プレイヤーのシーズンでのデータを取得する
-  def get_player_season_stats(api_key, player_id, season_id)
-    url = BASE_URL + "players/" + player_id + "/seasons/" + season_id.to_s
-    return request_data(api_key, url)
-  end
-  module_function :get_player_season_stats
+    #プレイヤーの通算のデータを取得する
+    def get_player_lifetime_stats(api_key, player_id)
+      url = BASE_URL + "players/" + player_id
+      return request_data(api_key, url)
+    end
 
-  #プレイヤーの通算のデータを取得する
-  def get_player_lifetime_stats(api_key, player_id)
-    url = BASE_URL + "players/" + player_id
-    return request_data(api_key, url)
+    #リクエストの送信
+    def request_data(api_key, url)
+      client = HTTPClient.new
+      header = [["Authorization", "Bearer #{api_key}"], ["Accept", "application/vnd.api+json"]]
+      return client.get(url, header: header)
+    end
   end
-  module_function :get_player_lifetime_stats
-
-  #リクエストの送信
-  def request_data(api_key, url)
-    client = HTTPClient.new
-    header = [["Authorization", "Bearer #{api_key}"], ["Accept", "application/vnd.api+json"]]
-    return client.get(url, header: header)
-  end
-  module_function :request_data
 end
