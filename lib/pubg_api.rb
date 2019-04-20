dad0ccbb9dcd48e22de63d60f085017666128557
Riki Ueno <31506@toyota.kosen-ac.jp>
2019-04-20 16:14:49 +0900

    PUBGApiをモジュールに変更 余計なメソッドの削除とそれに伴うコードの変更

--- lib/pubg_api.rb
+++ lib/pubg_api.rb
@@ -1,42 +1,44 @@
 require 'httpclient'
-require_relative './shape'
 
 module PUBGRateBot
-  class PUBGApi
-    @@season_id = nil
-    BASE_URL =  "https://api.pubg.com/shards/steam/"
-    
-    class << self
-      def feach_player_state(pubg_name)
-        url  =  BASE_URL + "players?filter[playerNames]=" + pubg_name
-        return request_data(url)
-      end
+  ### APIに関するモジュール
+  module PUBGApi
+    BASE_URL =  "https://api.pubg.com/shards/steam"
 
-      def feach_player_season_state(player_id)
-        url  =  BASE_URL + "players/" + player_id + "/seasons/" + @@season_id.to_s
-        return request_data(url)
-      end
+    # APIサーバからJSONを取得する
+    # @param [String] player_name プレイヤーネーム
+    # @return [String] リクエスト結果のJSON
+    def fetch_from_players(player_name)
+      url = "#{BASE_URL}/players?filter[playerNames]=#{player_name}"
+      http_request(url)
+    end
 
-      def feach_season
-        url  =  BASE_URL + "seasons"
-        data =  request_data(url)
-        @@season_id = Shape.extract_season_id(data) if data.status == 200
-      end
+    # APIサーバからJSONを取得する
+    # @return [String] リクエスト結果のJSON
+    def fetch_from_seasons
+      url = "#{BASE_URL}/seasons"
+      http_request(url)
+    end
 
-      def season_id
-        @@season_id
-      end
-    
-      def season_id=(val)
-        @@season_id = val
-      end
+    # APIサーバからJSONを取得する
+    # @param [String] player_id ゲーム内ID
+    # @param [String] season_id シーズンID
+    # @return [String] リクエスト結果のJSON
+    def fetch_from_seasons_stats(player_id, season_id)
+      url = "#{BASE_URL}/players/#{player_id}/seasons/#{season_id}"
+      http_request(url)
+    end
 
-      private
-      def request_data(url)
-        client = HTTPClient.new
-        header = [["Authorization", "Bearer #{API_KEY}"], ["Accept", "application/vnd.api+json"]]
-        return client.get(url, header: header)
-      end
+    private
+
+    # APIキーをヘッダーに埋め込んでurlに対してgetリクエストを行う
+    # @param [String] url リクエスト先のURL
+    # @return [String] リクエスト結果のJSON
+    #
+    def http_request(url)
+      client = HTTPClient.new
+      header = [["Authorization", "Bearer #{API_KEY}"], ["Accept", "application/vnd.api+json"]]
+      client.get(url, header: header)
     end
   end
 end
